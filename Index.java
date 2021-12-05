fb76aacebb52a0fa890b2c1a71cddb2976c921c0
sai teja <teja3821@gmail.com>
2021-12-05 12:39:43 -0600

    Update Index.java

--- Index.java
+++ Index.java
@@ -6,97 +6,67 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.SortedMap;
-public class Index{
-public static void createIndex(String tableName,String colName, String dType)
-{
+public class Index {
+    public static void createIndex(String tblName, String columnName, String dtaType) {
+        //System.out.println(tblName);
+        //System.out.println(columnName);
+        int serlCode = 0;
+        int recrdSize = 0;
+        if (dtaType.equalsIgnoreCase("int")) {
+            recrdSize = recrdSize + 4;
+            serlCode = 0x06;
+        } else if (dtaType.equalsIgnoreCase("tinyint")) {
+            recrdSize = recrdSize + 1;
+            serlCode = 0x04;
+        } else if (dtaType.equalsIgnoreCase("smallint")) {
+            recrdSize = recrdSize + 2;
+            serlCode = 0x05;
+        } else if (dtaType.equalsIgnoreCase("bigint")) {
+            recrdSize = recrdSize + 8;
+            serlCode = 0x07;
+        } else if (dtaType.equalsIgnoreCase("real")) {
+            recrdSize = recrdSize + 4;
+            serlCode = 0x08;
+        } else if (dtaType.equalsIgnoreCase("double")) {
+            recrdSize = recrdSize + 8;
+            serlCode = 0x09;
+        } else if (dtaType.equalsIgnoreCase("datetime")) {
+            recrdSize = recrdSize + 8;
+            serlCode = 0x0A;
+        } else if (dtaType.equalsIgnoreCase("date")) {
+            recrdSize = recrdSize + 8;
+            serlCode = 0x0B;
+        } else if (dtaType.equalsIgnoreCase("text")) {
+            //recrdSize=recrdSize+8;
+            serlCode = 0x0C;
+        }
+        int ordinalPosition1 = 0;
+        ArrayList < String > ordPos = new ArrayList < > ();
 
-//System.out.println(tableName);
-//System.out.println(colName);
-int serialCode = 0;
-int record_size = 0;
- if(dType.equalsIgnoreCase("int"))
-        {
-            record_size=record_size+4;
-            serialCode=0x06;
+        for (int oP = 0; oP < 16; oP++) {
+            ordPos.add(".");
         }
-        else if(dType.equalsIgnoreCase("tinyint"))
-        {
-            record_size=record_size+1;
-            serialCode=0x04;
-        }
-        else if(dType.equalsIgnoreCase("smallint"))
-        {
-            record_size=record_size+2;
-            serialCode=0x05;
-        }
-        else if(dType.equalsIgnoreCase("bigint"))
-        {
-            record_size=record_size+8;
-            serialCode=0x07;
-        }
-        else if(dType.equalsIgnoreCase("real"))
-        {
-            record_size=record_size+4;
-            serialCode=0x08;
-        }
-        else if(dType.equalsIgnoreCase("double"))
-        {
-            record_size=record_size+8;
-            serialCode=0x09;
-        }
-        else if(dType.equalsIgnoreCase("datetime"))
-        {
-            record_size=record_size+8;
-            serialCode=0x0A;
-        }
-        else if(dType.equalsIgnoreCase("date"))
-        {
-            record_size=record_size+8;
-            serialCode=0x0B;
-        }
-        else if(dType.equalsIgnoreCase("text"))
-        {
-            //record_size=record_size+8;
-            serialCode=0x0C;
-        }
-                         int ordinal_pos1 = 0;
- ArrayList<String> ord_position = new ArrayList<>();
- 
- 
 
+        try {
+            RandomAccessFile columnFile1 = new RandomAccessFile("data/" + columnName + ".ndx", "rw");
+            columnFile1.setLength(512);
 
-                         for(int o=0;o<16;o++){
-                                ord_position.add(".");
-                            }
+            columnFile1.seek(0);
+            columnFile1.writeByte(serlCode);
+            int null_val = 1;
 
+            columnFile1.writeByte(null_val);
+            //System.out.println(ordinalPosition1);
 
-try{
-                
-                    RandomAccessFile colFile1=new RandomAccessFile("data/"+colName+".ndx", "rw");
-                     colFile1.setLength(512);
-                                            colFile1.seek(0);
-                                            colFile1.writeByte(serialCode);
-                                            int null_val = 1;
-
-                                           
-                                            colFile1.writeByte(null_val);
-                                            //System.out.println(ordinal_pos1);
-
-                                            colFile1.write(ordinal_pos1);
-                                            ordinal_pos1++;
-                                            colFile1.writeByte(0x00);
-                                            colFile1.writeByte(0x10);
-                   // nmc.seek(2);
-                    //int position=nmc.read();
-                    //ord_position.set(position, colName);
-                    }
-                    catch (Exception e)
-                    {
-                    
-                    System.out.println(e);
-                    
-                    }    
-
+            columnFile1.write(ordinalPosition1);
+            ordinalPosition1++;
+            columnFile1.writeByte(0x00);
+            columnFile1.writeByte(0x10);
+            // nmc.seek(2);
+            //int position=nmc.read();
+            //ordPos.set(position, columnName);
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
 }
-}
-
